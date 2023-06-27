37bafe745db22d18207db679145a9e5f01ce58e0
ZoeHancox <83767921+ZoeHancox@users.noreply.github.com>
2023-06-27 20:40:54 +0100

    Delete test_activations_hypothesis.py

--- Pytesting/test_activations_hypothesis.py
+++
@@ -1,9 +0,0 @@
-import activations
-from hypothesis import given
-from hypothesis.strategies import floats
-
-
-@given(floats())
-def test_sigmoid(x):
-    result = activations.sigmoid(x)
-    assert 0 <= result <= 1, "Value not between 0 & 1"
